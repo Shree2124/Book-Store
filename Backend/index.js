b554adfba365336da599292b52ae4e41c3a775f4
Shree2124 <shreealasande@gmail.com>
2024-03-02 14:02:40 +0530

    commit 05 (CORS Error fixed and Frontend)

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,18 @@ import mongoose from "mongoose";
 import { PORT, PROJECT_ID } from "./config.js";
 import { Book } from "./models/books.models.js";
 import booksRoutes from './routes/books.routes.js'
+import cors from "cors"
 
 
 const app = express();
 const Port = PORT || 3000;
 
 app.use(express.json({ limit: "16kb" }));
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ['GET', 'POST', 'PUT', 'DELET'],
+  allowedHeaders: ['Contenet-Type'],
+}));
 app.use('/books',booksRoutes);
 
 app.get("/", (req, res) => {
